6340ac2fb5391889b5cb5a44c5f38a2de4ff0569
heather-han <heather.h.1122@gmail.com>
2019-04-05 10:09:56 -0400

    output labels to file

--- generate_labels.sh
+++ generate_labels.sh
@@ -2,10 +2,12 @@
 
 files=($(ls *.csv))
 
+rm "time_labels.log"
+
 # sort by 2nd column
 sort_times=()
 for ((i=0;i<${#files[@]};i++)); do
 	sort_time="$(time ( sort -k2 ${files[i]}) 2>&1 1>/dev/null )"
 	sort_times=( ${sort_times[@]} sort_time )
-	echo "${files[i]}: $sort_time"
+	echo "${files[i]}: $sort_time" >> "time_labels.log"
 done
