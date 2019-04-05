9105ec29520dd6eca9e18a12dfc8967e0bf383e8
heather-han <heather.h.1122@gmail.com>
2019-04-05 10:23:07 -0400

    print current file

--- generate_labels.sh
+++ generate_labels.sh
@@ -7,6 +7,7 @@ rm "time_labels.log"
 # sort by 2nd column
 sort_times=()
 for ((i=0;i<${#files[@]};i++)); do
+	echo "Sorting ${files[i]}..."
 	sort_time="$(time ( sort -k2 ${files[i]}) 2>&1 1>/dev/null )"
 	sort_times=( ${sort_times[@]} sort_time )
 	echo "${files[i]}: $sort_time" >> "time_labels.log"
