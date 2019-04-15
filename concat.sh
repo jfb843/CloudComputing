40b8cadbd4e5b7c34667e07e7d7749646011ea4b
Sharmila Tamby <stamby1@jhu.edu>
2019-04-15 15:12:32 -0400

    fix typos

--- concat.sh
+++ concat.sh
@@ -2,7 +2,8 @@
 
 # all_files=$(find ~/CloudComputing/data/*.csv -type f -size -2000000c)
 
-mkdir "compiled_data/"
+mkdir -p "compiled_data/"
+echo "Created compiled_data folder..."
 
 # concat the mortality files
 find ~/CloudComputing/data -type f -name '20*_data.csv' -exec cat {} + > ~CloudComputing/data/20x_data-merged.csv
@@ -14,12 +15,13 @@ echo "Number of files ${#all_files[@]}..."
 
 for ((i=0;i<${#all_files[@]};i++)); do
 	echo "Concatenating files to ${all_files[i]}..."
-	for ((j=$i;j<${#all_files[@]};j++)); do
+	for ((j=$((i+1));j<${#all_files[@]};j++)); do
 		# extract filenames for easy output file name
 		first_name=$(basename "${all_files[i]}" .csv)
 		second_name=$(basename "${all_files[j]}" .csv)
 
-		join -t, ${all_files[i]} ${all_files[j]} > ~/CloudComputing/compiled_data/${first_name}-${second_name}.csv
+		#join -t, ${all_files[i]} ${all_files[j]} > ~/CloudComputing/compiled_data/${first_name}-${second_name}.csv
+		paste -d, ${all_files[i]} ${all_files[j]} > ~/CloudComputing/compiled_data/${first_name}-${second_name}.csv
 		size=$(stat -c %s ~/CloudComputing/data/${first_name}-${second_name}.csv)
 		cols=$(awk '{print NF}' ~/CloudComputing/data/${first_name}-${second_name}.csv | sort -nu | tail -n 1)
 		rows=$(cat ~/CloudComputing/data/${first_name}-${second_name}.csv | wc -l)
--- duplicate.sh
+++ duplicate.sh
@@ -2,7 +2,8 @@
 
 # all_files=$(find ~/CloudComputing/data/*.csv -type f -size -2000000c)
 
-mkdir "compiled_data/"
+mkdir -p "compiled_data/"
+echo "Created compiled_data folder..."
 
 all_files=(~/CloudComputing/data/*.csv)
 echo "Number of files ${#all_files[@]}..."
@@ -15,21 +16,21 @@ duplicate () {
 		name=$(basename "${all_files[i]}" .csv)
 
 		# duplicate all files once
-		cat ${all_files[i]} ${all_files[i]}  > ~/CloudComputing/data/${name}-2.csv
-		size=$(stat -c %s ~/CloudComputing/data/${name}-2.csv)
+		cat ${all_files[i]} ${all_files[i]}  > ~/CloudComputing/data/${name}-concat.csv
+		size=$(stat -c %s ~/CloudComputing/data/${name}-concat.csv)
 		echo "${size}"
 		
 		# if still not sufficiently large
 		declare -i count=2 # file currently duplicated
 		while [ "${size}" -lt $1 ]; do
 			count=$((count + 1))
-			cat ~/CloudComputing/data/${name}-$((count - 1)).csv ~/CloudComputing/data/${name}-$((count - 1)).csv > ~/CloudComputing/data/${name}-${count}.csv
-			size=$(stat -c %s ~/CloudComputing/data/${name}-${count}.csv)
-			echo "File number $i size is... "${size}""
+			cat ~/CloudComputing/data/${name}-concat.csv ${all_files[i]} > ~/CloudComputing/data/${name}-concat.csv
+			size=$(stat -c %s ~/CloudComputing/data/${name}-concat.csv)
+			echo "File number $i with count $count: size is... "${size}""
 		done
 
-		mv ~/CloudComputing/data/${name}-${count}.csv ~/CloudComputing/compiled_data/${name}-${count}-$2.csv	
-		rm ~/CloudComputing/data/${name}-*.csv # remove the old files
+		mv ~/CloudComputing/data/${name}-concat.csv ~/CloudComputing/compiled_data
+		mv ~/CloudComputing/compiled_data/${name}-concat.csv ~/CloudComputing/compiled_data/${name}-${count}-$2.csv
 	done
 }
 
