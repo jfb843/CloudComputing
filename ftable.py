a84730ae8732afdb4658113feab7dc1e9b04dca2
jfb843 <jfb843@gmail.com>
2019-04-15 23:21:07 +0000

    updated feature space

---
+++ ftable.py
@@ -0,0 +1,8 @@
+import csv
+import pandas as pd
+
+with open('size_row_cols.log', 'r') as logfile, open('feature_table.csv', 'w') as csvfile:
+    reader = csv.reader(logfile, delimiter=' ')
+    writer = csv.writer(csvfile)
+    writer.writerow(['filename', 'size', 'rows', 'cols'])
+    writer.writerows(reader)
