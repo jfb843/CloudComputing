d3eb5779d1a9994ecaf3a7b879db8537b9d74827
Sharmila Tamby <stamby1@jhu.edu>
2019-04-17 19:39:31 -0400

    generating sort max min labels

--- generate_labels.sh
+++ generate_labels.sh
@@ -1,14 +1,29 @@
 #!/usr/bin/env bash
 
-files=($(ls "data/"*.csv))
+new_files=(~/CloudComputing/compiled_data/*.csv)
+old_files=(~/CloudComputing/data/*.csv)
+files=( ${old_files[@]} ${new_files[@]} )
+
+#files=($(ls "data/"*.csv))
 
 rm "time_labels.log"
 
-# sort by 2nd column
-sort_times=()
+echo "${#files[@]}"
+
 for ((i=0;i<${#files[@]};i++)); do
+	# sort by 2nd column
 	echo "Sorting ${files[i]}..."
 	sort_time="$(time ( sort -k2 ${files[i]}) 2>&1 1>/dev/null )"
-	sort_times=( ${sort_times[@]} sort_time )
-	echo "${files[i]}: $sort_time" >> "time_labels.log"
+
+	# max by 3nd column
+	echo "Calculating max ${files[i]}..."
+	max_time="$(time ( awk  'BEGIN{max=-1000}{if(($3)>max) max=($3)}END {print max}' ${files[i]}) 2>&1 1>/dev/null )"
+
+	# min by 3nd column
+	echo "Calculating min ${files[i]}..."
+	min_time="$(time ( awk  'BEGIN{min=1000000}{if(($3)>min)  min=($3)}END {print min}' ${files[i]}) 2>&1 1>/dev/null )"
+
+	echo "${files[i]}: $sort_time $max_time $min_time" >> "time_labels.log"
 done
+
+python3 ltable.py
