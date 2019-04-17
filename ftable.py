4c115a4cadf4dc250a15008466fd4eb40fb3ad02
Sharmila Tamby <stamby1@jhu.edu>
2019-04-17 17:20:43 -0400

    removed useless preprocessing scripts

--- ftable.py
+++ ftable.py
@@ -1,7 +1,7 @@
 import csv
 import pandas as pd
 
-with open('size_row_cols.log', 'r') as logfile, open('feature_table.csv', 'w') as csvfile:
+with open('size_rows_cols.log', 'r') as logfile, open('feature_table.csv', 'w') as csvfile:
     reader = csv.reader(logfile, delimiter=' ')
     writer = csv.writer(csvfile)
     writer.writerow(['filename', 'size', 'rows', 'cols'])
