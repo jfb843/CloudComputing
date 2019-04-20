04fe9a59bb46d5e880cbc8d63a45ec036f7245a3
Heather Han <Heather.h.1122@gmail.com>
2019-04-20 10:35:49 -0400

    Update 27_script.sh
    feature_tbl-merged.csv does not exist

--- scripts/27_script.sh
+++ scripts/27_script.sh
@@ -1,2 +1,2 @@
 awk 'BEGIN { max=0 } $27 > max { max=$27; name=$27 } END { print name }' FS="," compiled_data/2010_data-merged.csv
-awk 'BEGIN { min=1000000 } $0 < min { min=$0; name=$0 } END { print name }' FS="," compiled_data/feature_tbl-merged.csv
+awk 'BEGIN { min=1000000 } $0 < min { min=$0; name=$0 } END { print name }' FS="," compiled_data/adult-merged.csv
