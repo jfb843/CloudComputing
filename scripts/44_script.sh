871aae5d2f2cc6b76a9743e5d58638fa60c4cf3d
Heather Han <Heather.h.1122@gmail.com>
2019-04-20 08:33:57 -0400

    Update 44_script.sh

--- scripts/44_script.sh
+++ scripts/44_script.sh
@@ -1,1 +1,1 @@
-awk 'BEGIN { min=1000000 } $0 < min { min=$0; name=$0 } END { print name }' FS="," data/feature_tbl.csv
+awk 'BEGIN { min=1000000 } $0 < min { min=$0; name=$0 } END { print name }' FS="," data/emails.csv
