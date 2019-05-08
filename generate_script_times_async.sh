333efe7e9151db4a1ca1f7cdf0315fcebe3501f6
heather-han <heather.h.1122@gmail.com>
2019-05-07 22:44:57 -0400

    async runtimes

--- generate_script_times_async.sh
+++ generate_script_times_async.sh
@@ -2,7 +2,7 @@
 
 # Author: Heather Han
 # Generate time labels for each script in different environments by
-# submitting each as part of a batch of 5-20 asynchronous jobs
+# submitting each as part of a batch of 2-10 asynchronous jobs
 
 # Usage: run this script locally and modify the "user defined" section for 
 # your cluster
@@ -23,10 +23,11 @@ for ((i=1;i<101;i++)); do
 	jar_files=( ${jar_files[@]} $path"script"$i".jar")
 done
 
-# 20 iterations of groups of asynchronous jobs
-for ((i=0;i<20;i++)); do
-	# select random number (in range 5-20) of scripts for the iter
-	num_scripts=`shuf -i 5-20 -n 1`
+# 25 iterations of groups of asynchronous jobs
+for ((i=1;i<26;i++)); do
+	echo "Iteration $i of 20..."
+	# select random number (in range 2-10) of scripts for the iter
+	num_scripts=`shuf -i 2-10 -n 1`
 	group_id=$i'-'$num_scripts
 
 	job_ids=()  # id of hadoop job
@@ -57,12 +58,18 @@ for ((i=0;i<20;i++)); do
 		echo "waiting for async job ${job_ids[j]} to complete..."
 		while [[ $output != *"state: DONE"* ]]; do
 			output=`gcloud dataproc jobs describe ${job_ids[j]} --project $project --region $region`
+			# ping every 20 seconds
+			if [[ $output != *"state: DONE"* ]]; then sleep 20;	fi
 		done
 		# save details of completed job
 		echo ${indices[j]} >> model/runtimes_async.log
 		echo $output >> model/runtimes_async.log
 	done
+
+	sleep 1m  # wait 1 min before starting next iter
 done
 
 # Helper function to get runtimes and save in csv file
+cd model
 python3 script_times_helper.py --asynchronous True
+cd - 
--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ source get_data.sh
 source feature_space.sh
 
 # 3) randomly generate scripts (create samples for the ML model)
-read -p "Do you want to generate new scripts (default is no - to use existing scripts)? (y/n)" yn
+read -p "Do you want to generate new scripts (default is no - to use existing scripts)? (y/n) " yn
 	case $yn in
 		yes|Yes|y|Y ) python3 generate_scripts.py;;
 		no|No|n|N )	echo "using exisitng scripts in scripts/";;
@@ -18,14 +18,14 @@ read -p "Do you want to generate new scripts (default is no - to use existing sc
 	esac
 
 # 4) execute and time generated scripts
-read -p "Do you want to run scripts synchronously or asynchronously (sync/async)? " method
+read -p "Do you want to run scripts synchronously or asynchronously? (sync/async) " method
 	case $method in
 		# 4a) sequential method
 		sync|Sync|synchronous|Synchronous ) source generate_script_times.sh;;
 		# 4b) update src/sample.java and src/sample.mf with user-specific paths and 
 		#    generate jar files
 		async|Async|asynchronous|Asynchronous ) 
-			read -p "Have you updated src/sample.java, src/sample.mf, and generate_script_times_async.sh (y/n)? " yn
+			read -p "Have you updated src/sample.java, src/sample.mf, and generate_script_times_async.sh? (y/n) " yn
 			 	case $yn in
 			 		yes|Yes|y|Y ) source compile.sh; echo "Execute generate_script_times_async.sh locally using source generate_script_times_async.sh";;
 					no|No|n|N ) echo "Please update files and re-execute!";;
@@ -36,4 +36,9 @@ read -p "Do you want to run scripts synchronously or asynchronously (sync/async)
     esac
 
 # 5) build and evaluate ML models
-python3 models.py
+read -p "Have you ran generate_script_times_async.sh to get labels? (y/n) " yn
+	case $yn in
+ 		yes|Yes|y|Y ) python3 model/models.py;;
+		no|No|n|N ) echo "Please generate labels first before running the model";;
+		* ) echo "invalid";;
+	esac
